e3c1411d6f2afbb545382f579598cc90d1aeb10a
Hekalo Tanya <hekalot@gmail.com>
2021-05-25 01:55:57 +0300

    added main routes to the app

--- api/initServer.js
+++ api/initServer.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const cors = require('cors');
+const { v4: uuidv4 } = require('uuid');
 const { PrismaClient } = require('@prisma/client');
 
 const initServer = async () => {
@@ -13,14 +14,97 @@ const initServer = async () => {
   app.use(express.json());
   app.use(express.static('build'));
 
-  app.get('/users', async (req, res) => {
-    const users = await prisma.users.findMany();
+  app.get('/chats', async (req, res) => {
+    const { userId } = req.body;
 
-    console.log(users);
+    const chats = await prisma.chats.findMany({
+      where: {
+        OR: {
+          recipient_id: userId,
+          sender_id: userId,
+        }
+      }
+    });
 
     await prisma.$disconnect();
 
-    res.send(users);
+    res.send(chats);
+  });
+
+  app.get('/chat/:id', async (req, res) => {
+    const { id } = req.params
+
+    const chat = await prisma.chats.findUnique({
+      where: { id }
+    });
+
+    await prisma.$disconnect();
+
+    res.send(chat);
+  });
+
+  app.post('/chat/:id', async (req, res) => {
+    const { id } = req.params;
+    const { message, authorId } = req.body
+
+    const message = await prisma.chatMessages.create({
+      data: {
+        chatId: id,
+        authorId,
+        message,
+      }
+    });
+
+    await prisma.$disconnect();
+
+    res.send(message);
+  });
+
+  app.post('/sign-up', async (req, res) => {
+    let user = null;
+    try {
+      user = await prisma.users.create({
+        data: {
+          token: uuidv4(),
+          ...req.body,
+        }
+      });
+
+      res.json({
+        id: user.id,
+        username: user.username,
+        token: user.token,
+      });
+
+    } catch (e) {
+      res.send(new Error(e));
+    } finally {
+      await prisma.$disconnect();
+    }
+  });
+
+  app.post('/sign-in', async (req, res) => {
+    const { username, password } = req.body;
+
+    let user = null;
+    try {
+      user = await prisma.users.findUnique({
+        where: {
+          username,
+          password,
+        }
+      });
+
+      res.json({
+        id: user.id,
+        username: user.username,
+        token: user.token,
+      });
+    } catch (e) {
+      res.send(new Error(e));
+    } finally {
+      await prisma.$disconnect();
+    }
   });
 
   return {
